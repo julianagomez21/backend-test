4e1dea10cdda4c0af569913a308830de430b172e
Juliana <juliana.gomez21225@ucaldas.edu.co>
2026-04-16 16:35:30 -0500

    Azure deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const app = require("./app");
 const { AppDataSource } = require("./data/data-source");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
   .then(() => {
